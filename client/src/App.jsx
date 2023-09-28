e4c7246e27826802bfbdd98210f750bdb0e99501
Ramsyz <seetaram.vannemreddi@gmail.com>
2023-09-28 08:58:12 -0400

    Created Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,16 @@ import About from "./pages/About";
 import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
+//components
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About/>} />
+        <Route path="/about" element={<About />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
